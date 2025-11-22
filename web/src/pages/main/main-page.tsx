a297d3d10a7c01b26d86671ddfe733b89ed0f4e4
Dubabbi <thdms3160@naver.com>
2025-11-22 23:11:17 +0900

    feat: button 컴포넌트 완성

--- web/src/pages/main/main-page.tsx
+++ web/src/pages/main/main-page.tsx
@@ -4,18 +4,11 @@ const Main = () => {
   return (
     <div className="text-gray-90 flex-col-center px-[2.4rem] pt-[5rem]">
       <div className="mt-[3.2rem] flex w-full flex-col gap-[1.6rem]">
-        <Button
-          title="고민 상담하기"
-          className="bg-blue-80 w-full text-white"
-        />
+        <Button title="고민 상담하기" className="bg-blue-80 text-white" />
 
-        <Button title="다음" className="w-full bg-[#B7D589] text-white" />
+        <Button title="다음" className="bg-[#B7D589] text-white" />
 
-        <Button
-          title="다음"
-          className="bg-gray-10 w-full text-gray-50"
-          disabled
-        />
+        <Button title="다음" className="bg-gray-10 text-gray-50" disabled />
 
         <Button title="공유하기" className="w-full bg-[#0846A5] text-white" />
       </div>

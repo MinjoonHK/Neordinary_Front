27cff10d36d28768c80ef2a294060a2c4ce7e39a
MinjoonHK <minjoon.park.hk@gmail.com>
2025-11-23 05:16:25 +0900

    로그인 페이지 추가

--- web/src/pages/login/login-page.tsx
+++ web/src/pages/login/login-page.tsx
@@ -1,5 +1,53 @@
+import { useMutation } from '@tanstack/react-query';
+import { useEffect } from 'react';
+
+import { axiosClient } from '@/shared/apis/axios-client';
+import KakaoBubbleIcon from '@/shared/assets/icons/kakao-bubble-icon.svg';
+import KakaoIcon from '@/shared/assets/icons/kakao-icon.svg';
+import Button from '@/shared/components/button/button';
+
 const Login = () => {
-  return <div>df</div>;
+  const loginMutation = useMutation({
+    mutationFn: (params: { code: string }) => {
+      return axiosClient.post('/member/kakao/doLogin', { code: params.code });
+    },
+  });
+
+  const KAKAO_API_KEY = import.meta.env.VITE_KAKAO_API_KEY;
+  const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI;
+
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+  useEffect(() => {
+    const code = new URLSearchParams(window.location.search).get('code');
+
+    if (code) {
+      loginMutation.mutate({ code });
+    }
+  }, []);
+
+  return (
+    <div>
+      <div className="mt-[16.5rem] text-center text-[5rem]">{`{서비스 명}`}</div>
+      <div className="mt-[6rem] px-[2.4rem] text-center text-[2.4rem]">
+        서비스 설명서비스 설명서비스 설명서비스 설명서비스 설명
+      </div>
+      <div className="mt-[41.8rem] p-[2rem]">
+        <div className="flex justify-center">
+          <img src={KakaoBubbleIcon} alt="Image not found" />
+        </div>
+        <Button
+          // onClick={() => loginMutation.mutate()}
+          onClick={() => {
+            window.location.href = kakaoURL;
+          }}
+          startIcon={KakaoIcon}
+          title="카카오톡으로 로그인"
+          className="bg-[#FFEB3B] py-[1.4rem] text-[1.6rem]"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Login;

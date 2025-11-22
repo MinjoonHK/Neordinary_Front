f3e6aa91e56fa1e3d87e74a4fc8da644c342e4b1
Dubabbi <thdms3160@naver.com>
2025-11-22 20:30:34 +0900

    text: 배포 테스트

--- web/src/pages/main/main-page.tsx
+++ web/src/pages/main/main-page.tsx
@@ -1,7 +1,9 @@
 const Main = () => {
   return (
     <div>
-      <h1 className="title text-gray-90 flex-row-center mt-10">메인페이지</h1>
+      <h1 className="title text-gray-90 flex-row-center mt-10">
+        메인페이지 배포테스트
+      </h1>
     </div>
   );
 };

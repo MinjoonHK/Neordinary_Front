3e04f469efbe317a05ef14b34c57c4e49e87cfe2
Dubabbi <thdms3160@naver.com>
2025-11-23 00:46:05 +0900

    feat: signup 페이지 추가

---
+++ web/src/pages/signup/signup-page.tsx
@@ -0,0 +1,7 @@
+import Button from '@components/button/button';
+
+const Signup = () => {
+  return <div>df</div>;
+};
+
+export default Signup;
--- web/src/shared/routes/router.tsx
+++ web/src/shared/routes/router.tsx
@@ -5,6 +5,7 @@ import { createBrowserRouter } from 'react-router-dom';
 const Layout = lazy(() => import('@layouts/layout'));
 const CardDetailPage = lazy(() => import('@pages/card/card-datail'));
 const LoginPage = lazy(() => import('@pages/login/login-page'));
+const SignupPage = lazy(() => import('@pages/signup/signup-page'));
 const MainPage = lazy(() => import('@pages/main/main-page'));
 
 export const router = createBrowserRouter([
@@ -15,6 +16,10 @@ export const router = createBrowserRouter([
       {
         path: ROUTES.LOGIN,
         element: <LoginPage />,
+      },
+      {
+        path: ROUTES.SIGNUP,
+        element: <SignupPage />,
       },
       {
         path: ROUTES.MAIN,
